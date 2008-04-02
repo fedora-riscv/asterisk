6b222836a76b59595f74a927b6b9ae0ec0ac3900
Jeffrey C. Ollie <jcollie@fedoraproject.org>
2008-04-02 23:26:01 +0000

    - Return to stripped tarballs since there's more non-free content in the     Asterisk tarballs than I thought.

--- asterisk-strip.sh
+++ asterisk-strip.sh
@@ -6,13 +6,8 @@ if [ ! -f asterisk-$version.tar.gz ]; then
     exit 1
 fi
 
-
 tar xf asterisk-$version.tar.gz
-rm asterisk-$version/codecs/codec_ilbc.c
-rm -rf asterisk-$version/codecs/ilbc
-rm asterisk-$version/codecs/ilbc_slin_ex.h
-rm asterisk-$version/codecs/slin_ilbc_ex.h
-rm asterisk-$version/formats/format_ilbc.c
+rm asterisk-$version/sounds/*.tar.gz
 tar czf asterisk-$version-stripped.tar.gz asterisk-$version
 rm -rf asterisk-$version
 
